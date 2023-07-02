f56f96adf33c41f64b1f21b9a386752ab8e5b5d4
Petras <petrosenas@gmail.com>
2023-07-03 00:15:33 +0300

    Initiate commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Racoon from './Components/003/Racoon';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello React</h1>
+        <Racoon />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Components/003/Racoon.jsx
@@ -0,0 +1,6 @@
+function Racoon() {
+    return (
+        <h1>Racoon</h1>
+    );
+}
+export default App;
