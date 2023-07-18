ac8e35e7d8b881893994ae21225636eff940cc02
Petras <petrosenas@gmail.com>
2023-07-18 13:53:09 +0300

    Include

---
+++ src/005.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Dogs4 from './Components/005/Dogs4';
+import Dogs5 from './Components/005/Dogs5';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Dogs4 />  
+        <Dogs5 />            
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import './App.scss';
-import Dogs4 from './Components/005/Dogs4';
-import Dogs5 from './Components/005/Dogs5';
+import "./buttons.scss";
+import { useEffect, useState } from "react";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Dogs4 />  
-        <Dogs5 />            
+        <h1>API</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col">Column</div>
+            <div className="card">
+              <div className="card-header">New client</div>
+              <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                Go somewhere
+              </div>
+              <div className="d-grid gap-2 col-6 mx-auto">
+                <button button="btn btn-primary" type="button">
+                  Button
+                </button>
+                <button button="btn btn-primary" type="button">
+                  Button
+                </button>
+              </div>
+            </div>
+            <div className="col">Column</div>
+            <div className="col">Column</div>
+          </div>
+        </div>
       </header>
     </div>
   );
